b148ccdc311b75007af1b712dd6a9ca579804393
didenkovv <slavatorrent@ukr.net>
2018-07-30 17:24:20 +0300

    copy file from ftp to loacl dic

--- FtpConnectMain.py
+++ FtpConnectMain.py
@@ -1,15 +1,20 @@
 from ftplib import FTP
+import os
 ftpServerHQ = FTP('localhost')
 ftpServerHQ.login(user = 'vdidenko', passwd = 'vdidenko123')
 ftpLocalFobler = 'D:/TEMP'
-directory = '/01'
 
-print("")
+### copy file ftom ftp ###
+ftpFilenameFromServer = "1.exe"
 
+#### name filename local file ###
+ftpFilenameLocal_ = "11111.exe"
+#
+local_filename = os.path.join("D:/" , ftpFilenameLocal_)
+### copy ftp file ###
+ftpServerHQ.retrbinary("RETR " + ftpFilenameFromServer, open(local_filename, 'wb').write)
+ftpServerHQ.close()
 
-
-#data = ftpServerHQ.retrlines('LIST')
-#print(data)
 
 
 
