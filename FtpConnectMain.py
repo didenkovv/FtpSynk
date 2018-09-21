3690d9ca4bd99fefe2ff2b8f1f5083ed26d9574c
didenkovv <slavatorrent@ukr.net>
2018-09-21 16:29:39 +0300

    go to home. new version connection

--- FtpConnectMain.py
+++ FtpConnectMain.py
@@ -4,18 +4,47 @@ ftpServerHQ = FTP('localhost')
 ftpServerHQ.login(user = 'vdidenko', passwd = 'vdidenko123')
 ftpLocalFobler = 'D:/TEMP'
 
-### copy file ftom ftp ###
+ftpServerHQ.retrlines("LIST")
+
+##################################################################
+### copy name file ftom ftp ###
 ftpFilenameFromServer = "1.exe"
 
 #### name filename local file ###
-ftpFilenameLocal_ = "11111.exe"
-#
-local_filename = os.path.join("D:/" , ftpFilenameLocal_)
+ftpFilenameLocal = "11111.exe"
+
+### local fobler from ftp ###
+ftpResultFromFtp = "D:/ftplocal"
+
+#def copyFile():
+listing = []
+ftpServerHQ.retrlines("LIST", listing.append)
+words = listing[0].split(None, 8)
+filename = words[-1].lstrip()
+
+local_filename = os.path.join(ftpResultFromFtp, filename)
+
 ### copy ftp file ###
-ftpServerHQ.retrbinary("RETR " + ftpFilenameFromServer, open(local_filename, 'wb').write)
+ftpServerHQ.retrbinary("RETR " + filename,  open(local_filename, 'wb').write, 8*1024)
+print(ftpServerHQ.retrlines('LIST'))
+#print(ftpServerHQ.nlst())
 ftpServerHQ.close()
 
+##################################################################
+#copyFile()
+#ftpServerHQ.cwd("")
+#ftpServerHQ.retrlines("LIST")
+#for files in os.walk("/"):
+#    for file in files:
+#        ftpServerHQ.retrbinary("RETR " + ftpFilenameFromServer, open(local_filename, 'wb').write)
 
-
-
-
+#import os
+import sys
+#import fnmatch
+#root = 'D:/TEMP'
+#pattern = '*.*'
+#for folder, subdirs, files in os.walk(root):
+   # print(folder)
+#    for filename in fnmatch.filter(files, pattern):
+#        fullname = os.path.join(folder, filename)
+#        print(fullname)
