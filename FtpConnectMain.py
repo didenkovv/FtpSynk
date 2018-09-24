c91200f1f2a045c106b3d6c0ffe6eeac4d51d62d
didenkovv <slavatorrent@ukr.net>
2018-09-24 17:35:37 +0300

    copy bach files

--- FtpConnectMain.py
+++ FtpConnectMain.py
@@ -1,50 +1,22 @@
-from ftplib import FTP
 import os
-ftpServerHQ = FTP('localhost')
-ftpServerHQ.login(user = 'vdidenko', passwd = 'vdidenko123')
-ftpLocalFobler = 'D:/TEMP'
+from ftplib import FTP
 
-ftpServerHQ.retrlines("LIST")
+ftp = FTP('localhost')
+ftp.login("user", "*****")
+ftp.retrlines("LIST")
 
-##################################################################
-### copy name file ftom ftp ###
-ftpFilenameFromServer = "1.exe"
+#ftp.cwd("D:\TEMP\---Q2-2018")
+#ftp.cwd("D:\TEMP\---Q2-2018")  # or ftp.cwd("folderOne/subFolder")
 
-#### name filename local file ###
-ftpFilenameLocal = "11111.exe"
-
-### local fobler from ftp ###
-ftpResultFromFtp = "D:/ftplocal"
-
-#def copyFile():
 listing = []
-ftpServerHQ.retrlines("LIST", listing.append)
+ftp.retrlines("LIST", listing.append)
 words = listing[0].split(None, 8)
 filename = words[-1].lstrip()
+filenames = ftp.nlst()
+print(filenames)
 
-local_filename = os.path.join(ftpResultFromFtp, filename)
-
-### copy ftp file ###
-ftpServerHQ.retrbinary("RETR " + filename,  open(local_filename, 'wb').write, 8*1024)
-print(ftpServerHQ.retrlines('LIST'))
-#print(ftpServerHQ.nlst())
-ftpServerHQ.close()
-
-##################################################################
-#copyFile()
-#ftpServerHQ.cwd("")
-#ftpServerHQ.retrlines("LIST")
-#for files in os.walk("/"):
-#    for file in files:
-#        ftpServerHQ.retrbinary("RETR " + ftpFilenameFromServer, open(local_filename, 'wb').write)
-
-#import os
-import sys
-#import fnmatch
-#root = 'D:/TEMP'
-#pattern = '*.*'
-#for folder, subdirs, files in os.walk(root):
-   # print(folder)
-#    for filename in fnmatch.filter(files, pattern):
-#        fullname = os.path.join(folder, filename)
-#        print(fullname)
+for filename in filenames:
+        local_filename = os.path.join(r"D:/ftplocal", filename)
+        lf = open(local_filename, "wb")
+        ftp.retrbinary("RETR " + filename, lf.write, 8 * 1024)
+        lf.close()
