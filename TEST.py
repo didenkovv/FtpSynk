1296482a3092c4280e42f7ad8e03d278fc75e49b
didenkovv <slavatorrent@ukr.net>
2018-09-24 16:55:57 +0300

    test for bach

---
+++ TEST.py
@@ -0,0 +1,24 @@
+import os
+from ftplib import FTP
+
+ftp = FTP('localhost')
+ftp.login("vdidenko", "vdidenko123")
+ftp.retrlines("LIST")
+
+#ftp.cwd("D:\TEMP\---Q2-2018")
+#ftp.cwd("D:\TEMP\---Q2-2018")  # or ftp.cwd("folderOne/subFolder")
+
+listing = []
+ftp.retrlines("LIST", listing.append)
+words = listing[0].split(None, 8)
+filename = words[-1].lstrip()
+filenames = ftp.nlst()
+print(filenames)
+
+for filename in filenames:
+        local_filename = os.path.join(r"D:/ftplocal", filename)
+        lf = open(local_filename, "wb")
+ftp.retrbinary("RETR " + filename, lf.write, 8 * 1024)
+lf.close()
+
+
